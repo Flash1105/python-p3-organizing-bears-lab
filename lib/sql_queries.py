7687e3147ebc44c9d5e1c96fce70f6169f13d8d5
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-07-30 14:39:54 -0400

    update

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,3 +1,4 @@
+# Query 1: Select all female bears and return their name and age
 select_all_female_bears_return_name_and_age = """
     SELECT
         name,
@@ -6,26 +7,40 @@ select_all_female_bears_return_name_and_age = """
     WHERE sex='F';
 """
 
-select_young_bears_return_name_and_temperament = """
+# Query 2: Select all bears' names and order them in alphabetical order
+select_all_bears_names_and_orders_in_alphabetical_order = """
     SELECT
-        name,
-        temperament
+        name
     FROM bears
-    WHERE age < 5;
+    ORDER BY name;
 """
 
-select_bears_by_colors_return_name_and_color = """
-    SELECT
-        name,
-        color
-    FROM bears
-    WHERE color IN ('Brown', 'Black');
-"""
-
-select_dead_bears_return_name_and_age = """
+# Query 3: Select all bears' names and ages that are alive and order them from youngest to oldest
+select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT
         name,
         age
     FROM bears
-    WHERE alive = 0;
+    WHERE alive = 1
+    ORDER BY age;
+"""
+
+# Query 4: Select the oldest bear and return its name and age
+select_oldest_bear_and_returns_name_and_age = """
+    SELECT
+        name,
+        age
+    FROM bears
+    ORDER BY age DESC
+    LIMIT 1;
+"""
+
+# Query 5: Select the youngest bear and return its name and age
+select_youngest_bear_and_returns_name_and_age = """
+    SELECT
+        name,
+        age
+    FROM bears
+    ORDER BY age
+    LIMIT 1;
 """
