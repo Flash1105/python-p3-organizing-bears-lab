04b60597acdc2d697e7321c13d989ddfecd7c741
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-07-30 14:35:41 -0400

    update

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,31 @@
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM bears
+    WHERE sex='F';
 """
 
-select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+select_young_bears_return_name_and_temperament = """
+    SELECT
+        name,
+        temperament
+    FROM bears
+    WHERE age < 5;
 """
 
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+select_bears_by_colors_return_name_and_color = """
+    SELECT
+        name,
+        color
+    FROM bears
+    WHERE color IN ('Brown', 'Black');
 """
 
-select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
-"""
-select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+select_dead_bears_return_name_and_age = """
+    SELECT
+        name,
+        age
+    FROM bears
+    WHERE alive = 0;
 """
